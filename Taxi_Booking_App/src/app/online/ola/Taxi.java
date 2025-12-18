279d1a46896f1c3a5a6defd7e11e0888d50c98f7
vijayakumar <vijayakumar1498.eproc@gmail.com>
2025-12-18 15:10:54 +0530

    New Entity has been added for booking and taxi.

--- Taxi_Booking_App/src/app/online/ola/Taxi.java
+++ Taxi_Booking_App/src/app/online/ola/Taxi.java
@@ -1,5 +1,47 @@
 package app.online.ola;
 
+import java.util.List;
+
 public class Taxi {
+	int id;
+	char intialPoint = 'A';
+	double totalEarnings;
+	List<Booking> bookingList;
+
+	public Taxi(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public char getIntialPoint() {
+		return intialPoint;
+	}
+
+	public void setIntialPoint(char intialPoint) {
+		this.intialPoint = intialPoint;
+	}
+
+	public double getTotalEarnings() {
+		return totalEarnings;
+	}
+
+	public void setTotalEarnings(double totalEarnings) {
+		this.totalEarnings = totalEarnings;
+	}
+
+	public List<Booking> getBookingList() {
+		return bookingList;
+	}
+
+	public void setBookingList(List<Booking> bookingList) {
+		this.bookingList = bookingList;
+	}
 
 }
