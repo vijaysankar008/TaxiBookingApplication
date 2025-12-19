66e2d234fc6300cbbaa5f3a0bc2153068e15131e
vijayakumar <vijayakumar1498.eproc@gmail.com>
2025-12-19 17:18:52 +0530

    New changes has been done for booking logics-Ref #04

--- Taxi_Booking_App/src/app/online/ola/BookingController.java
+++ Taxi_Booking_App/src/app/online/ola/BookingController.java
@@ -1,11 +1,36 @@
 package app.online.ola;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BookingController {
-    
+    static List<Taxi> taxisList=new ArrayList<>();
     public static void main(String[] args) {
-        
+        Scanner userInput=new Scanner(System.in); 
+        System.out.println("Add How many taxi you want");
+        int taxiCount=userInput.nextInt();
+        createTaxiList(taxiCount);
+
         System.out.println("***Welcome to Taxi Booking Application***");
         System.out.println("Select Any Options 1.Taxi Booking");
         
+        int selectOption=userInput.nextInt();
+        switch (selectOption) {
+            case 1:{
+                System.out.println("You have selected the Booking option");
+            }
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+    public static void createTaxiList(int count){
+        for(int i=1;i<=count;i++){
+            taxisList.add(new Taxi(i));
+        }
     }
 }
