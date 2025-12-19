3fc2fafc4cc2b435a3d6766842af86c8397ae4de
vijayakumar <vijayakumar1498.eproc@gmail.com>
2025-12-19 12:02:43 +0530

    Controller has been added

---
+++ Taxi_Booking_App/src/app/BookingController.java
@@ -0,0 +1,11 @@
+package app;
+
+public class BookingController {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("***Welcome to Taxi Booking Application***");
+        System.out.println("Select Any Options 1.Taxi Booking");
+        
+    }
+}
