0ccfbe4ad3f6b0845c59473628b705551d77bfc4
vijayakumar <vijayakumar1498.eproc@gmail.com>
2025-12-22 14:16:03 +0530

    New Changes has been done on booking fuctionality_22_12

--- Taxi_Booking_App/src/app/online/ola/BookingController.java
+++ Taxi_Booking_App/src/app/online/ola/BookingController.java
@@ -3,29 +3,45 @@ package app.online.ola;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class BookingController {
+    static Scanner userInput=new Scanner(System.in); 
     static List<Taxi> taxisList=new ArrayList<>();
+
     public static void main(String[] args) {
-        Scanner userInput=new Scanner(System.in); 
+        
         System.out.println("Add How many taxi you want");
         int taxiCount=userInput.nextInt();
         createTaxiList(taxiCount);
-
         System.out.println("***Welcome to Taxi Booking Application***");
         System.out.println("Select Any Options 1.Taxi Booking");
         
         int selectOption=userInput.nextInt();
         switch (selectOption) {
             case 1:{
-                System.out.println("You have selected the Booking option");
-            }
-                
+                bookTaxi();
                 break;
+            }
         
             default:
                 break;
         }
+    }
+
+    public static void bookTaxi(){
+
+        System.out.println("Enter the pickup location");
+        char pickupLocation=userInput.next().charAt(0);
+        System.out.println("Enter the drop location");
+        char dropLocation=userInput.next().charAt(0);
+        System.out.println("Enter the pickup time");
+        int pickupTime=userInput.nextInt();
+
+        List<Taxi> availableTaxis=taxisList.stream().filter(taxi->taxi.taxiIsAvailable(pickupTime))
+                           .collect(Collectors.toList());
+
+        
     }
 
     public static void createTaxiList(int count){
